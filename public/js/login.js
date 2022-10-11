28b7a7834152726792491fd8f462cb38da69f663
Kaidan <kaidangustave@yahoo.com>
2022-10-10 20:51:01 -0500

    added login flow

--- public/js/login.js
+++ public/js/login.js
@@ -3,5 +3,30 @@ const form = document.getElementById('loginForm');
 const inputUsername = document.getElementById('inputUsername');
 const inputPassword = document.getElementById('inputPassword');
 
-const usernameFeedback = document.getElementById('usernameFeedback');
-const passwordFeedback = document.getElementById('passwordFeedback');
+const incorrectUserPass = document.getElementById('incorrectUserPass');
+
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: inputUsername.value.trim(),
+            password: inputPassword.value.trim()
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if(response.ok) {
+        document.location.replace('/');
+        return;
+    }
+
+    const json = await response.json();
+    if(json.message) {
+        incorrectUserPass.textContent = 'Incorrect username or password!';
+        incorrectUserPass.classList.remove('invisible');
+    } else {
+        alert('Failed to register!');
+    }
+});
