281d94805032449b9ad4b691c93bc6e21309468a
Kaidan Gustave <kaidangustave@yahoo.com>
2022-10-24 10:32:21 -0500

    deployment heroku

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,18 @@ const Sequelize = require('sequelize');
 
 const logging = process.env.DB_LOGS === 'true' ? console.log : (..._) => {};
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST || 'localhost',
-        dialect: 'mysql',
-        port: process.env.DB_PORT || 3306,
-        logging: logging
-    }
-);
+const sequelize = process.env.JAWSDB_URL ?
+    new Sequelize(process.env.JAWSDB_URL) :
+    new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASS,
+        {
+            host: process.env.DB_HOST || 'localhost',
+            dialect: 'mysql',
+            port: process.env.DB_PORT || 3306,
+            logging: logging
+        }
+    );
 
 module.exports = sequelize;
